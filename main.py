758934e177482e4cacc23defe5fde8fcf1ec86c6
diegosilvadef <diegofrancoh19@gmail.com>
2026-09-06 16:50:38 -0400

    first commit

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ def run_pipeline():
         # 1. Extração (Pega os últimos x dias úteis)
         df_dim, df_fato = extrair_dados_b3(period="1d")
 
-        print(df_dim.head(5))
-        print("--------------------------------")
-        print(df_fato.head(5))
+        # print(df_dim.head(5))
+        # print("--------------------------------")
+        # print(df_fato.head(5))
 
-        # # 2. Atualização da Dimensão
-        # atualizar_dimensao_acao(df_dim)
+        # 2. Atualização da Dimensão
+        atualizar_dimensao_acao(df_dim)
 
-        # # 3. Carga Incremental na Fato
-        # carregar_fato_acao(df_fato)
+        # 3. Carga Incremental na Fato
+        carregar_fato_acao(df_fato)
 
         print("=== PIPELINE FINALIZADO COM SUCESSO ===")
 
